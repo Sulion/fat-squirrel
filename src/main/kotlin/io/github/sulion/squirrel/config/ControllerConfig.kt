3085d5e878dc8c9a52cd4587d715098d89041920
Sulion <michael.rykov@gmail.com>
2019-11-24 00:18:54 +0300

    Add this file as explicit Heroku requirement

--- src/main/kotlin/io/github/sulion/squirrel/config/ControllerConfig.kt
+++ src/main/kotlin/io/github/sulion/squirrel/config/ControllerConfig.kt
@@ -43,7 +43,8 @@ class ControllerConfig {
                             SET_PRIVATE_FIELDS_CONVENTION
                         )
                     )
-                    .automatic(true).build()
+                    .automatic(true)
+                    .build()
             )
         )
     }
