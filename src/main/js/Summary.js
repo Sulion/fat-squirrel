7e4b0bac174827ea095edd8a97169e2e480b3331
Sulion <michael.rykov@gmail.com>
2019-11-23 18:52:20 +0300

    Make color strip dynamic

--- src/main/js/Summary.js
+++ src/main/js/Summary.js
@@ -6,6 +6,11 @@ import sumBy from "lodash/sumBy"
 
 
 function Summary(data) {
+    const totalProteins = sumBy(data.data, 'proteins');
+    const totalFats = sumBy(data.data, 'fats');
+    const totalCarbs = sumBy(data.data, 'carbs');
+    const eps = (window.innerWidth / 10.0 * 3.5) / (totalProteins + totalFats + totalCarbs);
+
     return (
         <React.Fragment>
             <Container className="summary">
@@ -17,7 +22,15 @@ function Summary(data) {
                 </Row>
                 <Row>
                     <Col className="col-7">&nbsp;</Col>
-                    <Col><hr/></Col>
+                    <Col className="col-sm-5">
+                        <svg width="35vw">
+                            <rect x="0" y="0" height="1vh" width={eps * totalProteins} fill="#F67100"/>
+                            <rect x={eps * totalProteins} y="0" height="1vh" width={eps * totalFats}
+                                  fill="#3D43F7"/>
+                            <rect x={eps * (totalProteins + totalFats)} y="0" height="1vh"
+                                  width={eps * totalCarbs} fill="#00B775"/>
+                        </svg>
+                    </Col>
                 </Row>
             </Container>
         </React.Fragment>
