cfc119e8ea2dae39f6de832015dffbdfc3c80153
Sulion <michael.rykov@gmail.com>
2019-11-24 00:16:33 +0300

    Make less nestedness

--- src/main/kotlin/io/github/sulion/squirrel/config/ControllerConfig.kt
+++ src/main/kotlin/io/github/sulion/squirrel/config/ControllerConfig.kt
@@ -2,6 +2,7 @@ package io.github.sulion.squirrel.config
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
+import com.mongodb.MongoClientSettings.getDefaultCodecRegistry
 import com.mongodb.client.MongoClients
 import com.typesafe.config.ConfigFactory
 import io.github.sulion.squirrel.controller.SearchController
@@ -9,6 +10,7 @@ import io.ktor.config.HoconApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
 import org.bson.codecs.configuration.CodecRegistries.fromProviders
 import org.bson.codecs.configuration.CodecRegistries.fromRegistries
+import org.bson.codecs.configuration.CodecRegistry
 import org.bson.codecs.pojo.Conventions.*
 import org.bson.codecs.pojo.PojoCodecProvider
 
@@ -24,24 +26,26 @@ class ControllerConfig {
         SearchController(
             MongoClientSettings.builder()
                 .applyConnectionString(ConnectionString(config.property(MONGODB_URI_PROPERTY).getString()))
-                .codecRegistry(
-                    fromRegistries(
-                        MongoClientSettings.getDefaultCodecRegistry(),
-                        fromProviders(
-                            PojoCodecProvider.builder()
-                                .conventions(
-                                    listOf(
-                                        ANNOTATION_CONVENTION,
-                                        CLASS_AND_PROPERTY_CONVENTION,
-                                        SET_PRIVATE_FIELDS_CONVENTION
-                                    )
-                                )
-                                .automatic(true).build()
-                        )
-                    )
-                )
+                .codecRegistry(codecRegistry())
                 .build()
                 .let { MongoClients.create(it) }
         )
+
+    private fun codecRegistry(): CodecRegistry {
+        return fromRegistries(
+            getDefaultCodecRegistry(),
+            fromProviders(
+                PojoCodecProvider.builder()
+                    .conventions(
+                        listOf(
+                            ANNOTATION_CONVENTION,
+                            CLASS_AND_PROPERTY_CONVENTION,
+                            SET_PRIVATE_FIELDS_CONVENTION
+                        )
+                    )
+                    .automatic(true).build()
+            )
+        )
+    }
 
 }
